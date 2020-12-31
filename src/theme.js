52bb76bfd2153df46a47c36056881b2d85e6cab3
Tim Lindgren <timothy.lindgren@bc.edu>
2020-12-30 20:44:38 -0600

    Adding Google Font - Inter
    https://fonts.google.com/specimen/Inter?sidebar.open=true&selection.family=Inter:wght@300;400;600;700;800#about

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react";
 
 /**
  * This theme object can be changed and edited however you like. It's currently extending the default Chakra theme
@@ -8,29 +8,33 @@ import { extendTheme } from '@chakra-ui/react'
 export const theme = extendTheme({
   colors: {
     brand: {
-      50: '#e2f3ff',
-      100: '#bedaf3',
-      200: '#98bfe6',
-      300: '#71a6d9',
-      400: '#4b8ccd',
-      500: '#3273b4',
-      600: '#25598d',
-      700: '#174066',
-      800: '#092640',
-      900: '#000e1c',
+      50: "#e2f3ff",
+      100: "#bedaf3",
+      200: "#98bfe6",
+      300: "#71a6d9",
+      400: "#4b8ccd",
+      500: "#3273b4",
+      600: "#25598d",
+      700: "#174066",
+      800: "#092640",
+      900: "#000e1c",
     },
   },
+  fonts: {
+    heading: "Inter",
+    body: "Inter",
+  },
   fontSizes: {
-    xs: '12px',
-    sm: '14px',
-    md: '16px',
-    lg: '18px',
-    xl: '20px',
-    '2xl': '24px',
-    '3xl': '28px',
-    '4xl': '36px',
-    '5xl': '48px',
-    '6xl': '64px',
+    xs: "12px",
+    sm: "14px",
+    md: "16px",
+    lg: "18px",
+    xl: "20px",
+    "2xl": "24px",
+    "3xl": "28px",
+    "4xl": "36px",
+    "5xl": "48px",
+    "6xl": "64px",
   },
   fontWeights: {
     normal: 400,
@@ -38,29 +42,29 @@ export const theme = extendTheme({
     bold: 700,
   },
   lineHeights: {
-    normal: 'normal',
-    none: '1',
-    shorter: '1.25',
-    short: '1.375',
-    base: '1.5',
-    tall: '1.625',
-    taller: '2',
+    normal: "normal",
+    none: "1",
+    shorter: "1.25",
+    short: "1.375",
+    base: "1.5",
+    tall: "1.625",
+    taller: "2",
   },
   letterSpacings: {
-    tighter: '-0.05em',
-    tight: '-0.025em',
-    normal: '0',
-    wide: '0.025em',
-    wider: '0.05em',
-    widest: '0.1em',
+    tighter: "-0.05em",
+    tight: "-0.025em",
+    normal: "0",
+    wide: "0.025em",
+    wider: "0.05em",
+    widest: "0.1em",
   },
-  breakpoints: ['30em', '48em', '62em', '80em'],
+  breakpoints: ["30em", "48em", "62em", "80em"],
 
   styles: {
     global: (props) => ({
       "html, body": {
         fontSize: "md",
-        color: props.colorMode === "dark" ? "white" : "gray.600",
+        color: props.colorMode === "dark" ? "white" : "Black",
         lineHeight: "tall",
       },
       a: {
@@ -68,8 +72,4 @@ export const theme = extendTheme({
       },
     }),
   },
-
-
-})
-
-
+});
