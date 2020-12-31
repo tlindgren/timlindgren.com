37aa7ff9a789c9b5bd9e05a78657af58e06cc485
Tim Lindgren <timothy.lindgren@bc.edu>
2020-12-31 16:43:22 -0600

    Adding social links component

---
+++ src/components/social.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Wrap, WrapItem, Button, Icon } from "@chakra-ui/react";
+import { FiTwitter, FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
+
+const Social = () => (
+  <Wrap spacing="2">
+    <WrapItem>
+      <Button
+        href="https://twitter.com/timlindgren"
+        as="a"
+        colorScheme="gray"
+        leftIcon={<Icon as={FiTwitter} />}
+      >
+        Twitter{" "}
+      </Button>
+    </WrapItem>
+    <WrapItem>
+      <Button
+        href="https://www.Buttonedin.com/in/timlindgren/"
+        as="a"
+        colorScheme="gray"
+        leftIcon={<Icon as={FiLinkedin} />}
+      >
+        LinkedIn
+      </Button>
+    </WrapItem>
+    <WrapItem>
+      <Button
+        href="https://www.instagram.com/timothylindgren/"
+        as="a"
+        colorScheme="gray"
+        leftIcon={<Icon as={FiInstagram} />}
+      >
+        {" "}
+        Instagram{" "}
+      </Button>
+    </WrapItem>
+    <WrapItem>
+      <Button
+        href="https://github.com/tlindgren"
+        as="a"
+        colorScheme="gray"
+        leftIcon={<Icon as={FiGithub} />}
+      >
+        Github
+      </Button>
+    </WrapItem>
+  </Wrap>
+);
+
+export default Social;
