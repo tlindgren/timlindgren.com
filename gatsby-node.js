95e874cae6b10c1959bd8f49841f155cbb5292df
Tim Lindgren <timothy.lindgren@bc.edu>
2020-12-31 00:13:51 -0600

    Adding gatsby-plugin-changelog-context and moment.js
    To show git commits and link to github page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
-const { createFilePath } = require("gatsby-source-filesystem")
+const { createFilePath } = require("gatsby-source-filesystem");
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
   // you only want to operate on `Mdx` nodes. If you had content from a
   // remote CMS you could also check to see if the parent node was a
   // `File` node here
   if (node.internal.type === "Mdx") {
-    const value = createFilePath({ node, getNode })
+    const value = createFilePath({ node, getNode });
     createNodeField({
       // Name of the field you are adding
       name: "slug",
@@ -15,53 +15,54 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
       // don't need a separating "/" before the value because
       // createFilePath returns a path with the leading "/".
       value: `${value}`,
-    })
+    });
   }
-}
+};
 
-const path = require("path")
+const path = require("path");
 exports.createPages = async ({ graphql, actions, reporter }) => {
   // Destructure the createPage function from the actions object
-  const { createPage } = actions
-  
+  const { createPage } = actions;
+
   const notes_query = await graphql(`
     query {
       allMdx(filter: { fileAbsolutePath: { regex: "/notes/" } }) {
         edges {
-            node {
-              id
-              fields {
-                slug
+          node {
+            id
+            fileAbsolutePath
+            fields {
+              slug
             }
           }
         }
       }
     }
-  `)
+  `);
   if (notes_query.errors) {
-    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
   }
 
   const pages_query = await graphql(`
     query {
       allMdx(filter: { fileAbsolutePath: { regex: "/pages/" } }) {
         edges {
-            node {
-              id
-              fields {
-                  slug
+          node {
+            id
+            fields {
+              slug
             }
           }
         }
       }
     }
-  `)
+  `);
   if (pages_query.errors) {
-    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query')
+    reporter.panicOnBuild('🚨  ERROR: Loading "createPages" query');
   }
 
   // Create note pages.
-  const notes = notes_query.data.allMdx.edges
+  const notes = notes_query.data.allMdx.edges;
   // you'll call `createPage` for each result
   notes.forEach(({ node }, index) => {
     createPage({
@@ -72,24 +73,26 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       component: path.resolve(`./src/components/note-template.js`),
       // You can use the values in this context in
       // our page layout component
+      context: {
+        id: node.id,
+        fileAbsolutePath: node.fileAbsolutePath,
+      },
+    });
+  });
+
+  // Create pages.
+  const pages = pages_query.data.allMdx.edges;
+  // you'll call `createPage` for each result
+  pages.forEach(({ node }, index) => {
+    createPage({
+      // This is the slug you created before
+      // (or `node.frontmatter.slug`)
+      path: node.fields.slug,
+      // This component will wrap our MDX content
+      component: path.resolve(`./src/components/page-template.js`),
+      // You can use the values in this context in
+      // our page layout component
       context: { id: node.id },
-    })
-  })
-
-   // Create pages.
-   const pages = pages_query.data.allMdx.edges
-   // you'll call `createPage` for each result
-   pages.forEach(({ node }, index) => {
-     createPage({
-       // This is the slug you created before
-       // (or `node.frontmatter.slug`)
-       path: node.fields.slug,
-       // This component will wrap our MDX content
-       component: path.resolve(`./src/components/page-template.js`),
-       // You can use the values in this context in
-       // our page layout component
-       context: { id: node.id },
-     })
-   })
-}
-
+    });
+  });
+};
--- src/components/note-template.js
+++ src/components/note-template.js
@@ -2,25 +2,46 @@ import React from "react";
 import { graphql } from "gatsby";
 import { MDXProvider } from "@mdx-js/react";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { Link, Heading, Box, Image, Button } from "@chakra-ui/react";
+import {
+  Link,
+  Heading,
+  Box,
+  Button,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  UnorderedList,
+  ListItem,
+} from "@chakra-ui/react";
 import SEO from "../components/seo";
+import moment from "moment";
 
-const shortcodes = { Link, Heading, Box, Image, Button }; // Provide common components here
+const shortcodes = { Link, Heading, Box, Button }; // Provide common components here
 
-export default function noteTemplate({ data }) {
+export default function noteTemplate({ data, pageContext }) {
   const { mdx } = data;
 
   return (
-    <article class="mdx">
+    <article>
       <SEO title={mdx.frontmatter.title} />
-      <Heading as="h1" size="2xl" my="8">
+      <Heading as="h1" size="2xl" fontWeight="800" my="10" mb="8">
         {mdx.frontmatter.title}
       </Heading>
-      <MDXProvider components={shortcodes}>
-        <MDXRenderer>{mdx.body}</MDXRenderer>
-      </MDXProvider>
+      <section class="mdx">
+        <MDXProvider components={shortcodes}>
+          <MDXRenderer>{mdx.body}</MDXRenderer>
+        </MDXProvider>
+      </section>
       <Box>
-        {mdx.inboundReferences.length > 0 ? <p>Referenced in:</p> : ""}
+        {mdx.inboundReferences.length > 0 ? (
+          <Heading as="h2" size="m">
+            Referenced In
+          </Heading>
+        ) : (
+          ""
+        )}
         <ul>
           {mdx.inboundReferences.map((ref) => (
             <li>
@@ -29,7 +50,30 @@ export default function noteTemplate({ data }) {
           ))}
         </ul>
       </Box>
-      Last Updated: {mdx.frontmatter.date}
+      <Accordion allowToggle mt="5">
+        <AccordionItem border="none" p="0">
+          <AccordionButton as="h2" size="xl" p="0">
+            <Heading as="h2" size="s" fontWeight="normal">
+              Revision History
+            </Heading>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4} px="0">
+            <UnorderedList mb="3">
+              {pageContext.changelog.map(({ hash, date, message, body }) => (
+                <ListItem key={hash}>
+                  {moment(date).format("ddd Do MMM, YYYY")}: {message} {body}
+                </ListItem>
+              ))}
+            </UnorderedList>
+            <Link
+              href={`https://github.com/tlindgren/timlindgren.com/blob/main/content/notes/${mdx.slug}.mdx`}
+            >
+              View in Github
+            </Link>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
     </article>
   );
 }
@@ -39,6 +83,7 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       id
       body
+      slug
       frontmatter {
         title
         date
