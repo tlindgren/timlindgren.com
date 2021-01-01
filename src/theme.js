d1f2b771aea9138c67d40c91567d562205685fdd
Tim Lindgren <timothy.lindgren@bc.edu>
2020-12-31 18:33:55 -0600

    adding fallback fonts

--- src/theme.js
+++ src/theme.js
@@ -22,8 +22,8 @@ export const theme = extendTheme({
     },
   },
   fonts: {
-    heading: "Inter",
-    body: "Inter",
+    heading: `"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+    body: `"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
   },
   fontSizes: {
     xs: "12px",
