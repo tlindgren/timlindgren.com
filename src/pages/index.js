da04fc851073974350989011aeec447849525232
Tim Lindgren <timothy.lindgren@bc.edu>
2021-01-02 23:29:06 -0600

    tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Box,
+  Icon,
   Text,
   Button,
   Center,
@@ -10,6 +11,7 @@ import {
 } from "@chakra-ui/react";
 import SEO from "../components/seo";
 import Social from "../../src/components/social";
+import { GoPlus } from "react-icons/go";
 
 const IndexPage = () => (
   <>
@@ -17,61 +19,84 @@ const IndexPage = () => (
     <Box as="section" pt="12" pb="0">
       <Center centerContent>
         <Text
-          fontSize="5xl"
+          fontSize={{ base: "5xl", lg: "5xl", xl: "6xl" }}
           fontWeight="extrabold"
           textAlign="center"
-          color="blue.900"
+          color="blue.600"
           m="0"
         >
-          learning + designing + thinking + living
+          learning <Icon as={GoPlus} color="brand.600" w={12} h={12} />{" "}
+          designing <Icon as={GoPlus} color="brand.600" w={12} h={12} />{" "}
+          thinking <Icon as={GoPlus} color="brand.600" w={12} h={12} /> living
         </Text>
       </Center>
-      <SimpleGrid columns={{ lg: "3" }} textAlign="center" spacing={4} pt="20">
-        <Box borderRadius="lg" boxShadow="lg" bgColor="brand.25">
+      <SimpleGrid
+        columns={{ lg: "3" }}
+        textAlign="center"
+        spacing={{ base: "10", md: "12", lg: "8", xl: "10" }}
+        pt="20"
+        m={{ base: "8", lg: "8", xl: "0" }}
+      >
+        <Box borderRadius="lg" boxShadow="md" bgColor="brand.25">
           <Text
             as="h2"
-            fontSize="2xl"
-            fontWeight="800"
+            fontSize={{ base: "3xl", md: "4xl", lg: "2xl", xl: "3xl" }}
+            fontWeight="400"
             textTransform="uppercase"
             mb="4"
             color="white"
-            bgColor="brand.700"
+            bgColor="brand.600"
           >
             Learning Design
           </Text>
-          <Text fontSize="xl" px="6" pb="6" pt="2">
+          <Text
+            fontSize={{ base: "xl", md: "2xl", lg: "xl", xl: "2xl" }}
+            px="6"
+            pb="6"
+            pt="2"
+          >
             Helping educators design meaningful learning experiences
           </Text>
         </Box>
-        <Box borderRadius="lg" boxShadow="lg" bgColor="brand.25">
+        <Box borderRadius="lg" boxShadow="md" bgColor="brand.25">
           <Text
             as="h2"
-            fontSize="2xl"
-            fontWeight="800"
+            fontSize={{ base: "3xl", md: "4xl", lg: "2xl", xl: "3xl" }}
+            fontWeight="400"
             textTransform="uppercase"
             mb="4"
             color="white"
-            bgColor="brand.700"
+            bgColor="brand.600"
           >
             Web Development
           </Text>
-          <Text fontSize="xl" px="6" pb="6" pt="2">
+          <Text
+            fontSize={{ base: "xl", md: "2xl", lg: "xl", xl: "2xl" }}
+            px="6"
+            pb="6"
+            pt="2"
+          >
             Building interfaces that show users some love
           </Text>
         </Box>
-        <Box borderRadius="lg" boxShadow="lg" bgColor="brand.25">
+        <Box borderRadius="lg" boxShadow="md" bgColor="brand.25">
           <Text
             as="h2"
-            fontSize="2xl"
-            fontWeight="800"
+            fontSize={{ base: "3xl", md: "4xl", lg: "2xl", xl: "3xl" }}
+            fontWeight="400"
             textTransform="uppercase"
             mb="4"
             color="white"
-            bgColor="brand.700"
+            bgColor="brand.600"
           >
             Consulting
           </Text>
-          <Text fontSize="xl" px="6" pb="6" pt="2">
+          <Text
+            fontSize={{ base: "xl", md: "2xl", lg: "xl", xl: "2xl" }}
+            px="6"
+            pb="6"
+            pt="2"
+          >
             Thinking with people to help their ideas grow
           </Text>
         </Box>
