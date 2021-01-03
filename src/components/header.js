a4b7f1550e7e657420eca55db9fda3d47e928174
Tim Lindgren <timothy.lindgren@bc.edu>
2021-01-02 23:27:57 -0600

    lightening header background

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Logo from "../../content/images/tl-logo-white.png"
-import { Container, Box, Flex, Heading, Link, Spacer, Image } from '@chakra-ui/react'
-import DrawerMenu from './drawer-menu'
-import HorizontalMenu from './horizontal-menu'
+import React from "react";
+import PropTypes from "prop-types";
+import Logo from "../../content/images/tl-logo-white.png";
+import {
+  Container,
+  Box,
+  Flex,
+  Heading,
+  Link,
+  Spacer,
+  Image,
+} from "@chakra-ui/react";
+import DrawerMenu from "./drawer-menu";
+import HorizontalMenu from "./horizontal-menu";
 
 const Header = ({ siteTitle }) => (
-  <Box as="header" background="brand.800" marginBottom="1.45rem">
+  <Box as="header" background="brand.700" marginBottom="1.45rem">
     <Container maxW="lg" py="3">
       <Flex align="center">
-        
-        <Image src={Logo} alt="" boxSize="50px" mr="3"/>
+        <Image src={Logo} alt="" boxSize="50px" mr="3" />
         <Heading as="h1" margin="0">
           <Link
             href="/"
             color="white"
             textTransform="uppercase"
-            _hover={{ textDecor: 'none' }}
+            fontWeight="600"
+            _hover={{ textDecor: "none" }}
           >
             {siteTitle}
           </Link>
@@ -26,17 +34,15 @@ const Header = ({ siteTitle }) => (
         <DrawerMenu />
       </Flex>
     </Container>
-
   </Box>
-  
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
